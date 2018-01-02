5078dc52559f3f60f6fb98f1ebb7ad6d6c8c930a
Daniel Kassler <dkassler@mathematica-mpr.com>
2018-01-02 18:59:05 +0000

    made master setup structure, added dotfiles

---
+++ get_dotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install yadm
+yadm clone https://github.com/dkassler/dotfiles.git
+yadm checkout aws_sae
+
--- setup.sh
+++ setup.sh
@@ -2,11 +2,8 @@
 #setup script for ubuntu on AWS using R, Stan
 strt=`date +%s`
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
-sudo apt-get update -y
-sudo apt-get install -y r-base
-sudo Rscript -e "install.packages('rstan', repos = 'https://cloud.r-project.org')"
+source install_R.sh
+source get_dotfiles.sh
 
 end=`date +%s`
 runtime=$((end-strt))
